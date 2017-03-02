9243f518eb0ad62f72f5175a2d80eba40598c60d
Ista Zahn <istazahn@gmail.com>
2017-03-02 18:45:14 +0000

    openblas

---
+++ bm1.R
@@ -0,0 +1,11 @@
+devtools::install_github("andrie/version.compare")
+library(version.compare)
+library("knitr")
+scale.factor <- 1.0
+
+r <- findRscript()
+
+test.results <- RevoMultiBenchmark(rVersions = r, 
+                                   threads = 1, 
+                                   scale.factor = scale.factor)
+
